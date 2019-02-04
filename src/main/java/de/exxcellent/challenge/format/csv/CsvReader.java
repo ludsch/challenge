42f7a5ea0d66eb97f34aaedf9f255e25f9eca4ae
Denis <denisschulze@gmx.net>
2019-02-04 17:34:07 +0100

    update csv reader

--- src/main/java/de/exxcellent/challenge/format/csv/CsvReader.java
+++ src/main/java/de/exxcellent/challenge/format/csv/CsvReader.java
@@ -1,5 +1,22 @@
 package de.exxcellent.challenge.format.csv;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CsvReader<T, U> {
 
+    private List<T> rows = new ArrayList<>();
+
+
+
+
+    public List<T> getRows() {
+        return rows;
+    }
+
+    private void addRow(T row) {
+        rows.add(row);
+    }
 }
+
+
