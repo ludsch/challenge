5382d3ba9b491908009ad459b1b5a4f598dd3690
Denis <denisschulze@gmx.net>
2019-02-04 18:13:26 +0100

    update parser test class

--- src/test/java/de/exxcellent/challenge/TestCsvReader.java
+++ src/test/java/de/exxcellent/challenge/TestCsvReader.java
@@ -21,6 +21,7 @@ public class TestCsvReader {
     public class Parsing {
         @BeforeEach
         public void createReader() {
+            csvReader = new CsvReader<>(new WeatherDataCsvRecordMapper());
         }
 
     }
