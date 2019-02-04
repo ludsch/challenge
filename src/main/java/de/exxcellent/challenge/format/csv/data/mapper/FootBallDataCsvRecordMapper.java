d4b08cb748cba83690a14a91fb183aadbed61f54
Denis <denisschulze@gmx.net>
2019-02-04 20:10:30 +0100

    implement football data record mapper

--- src/main/java/de/exxcellent/challenge/format/csv/data/mapper/FootBallDataCsvRecordMapper.java
+++ src/main/java/de/exxcellent/challenge/format/csv/data/mapper/FootBallDataCsvRecordMapper.java
@@ -7,6 +7,9 @@ import java.util.Map;
 public class FootBallDataCsvRecordMapper implements CsvRecordMapper<FootBallData> {
     @Override
     public FootBallData map(Map<String, String> record) {
-        return null;
+        int goals = Integer.parseInt(record.get("Goals"));
+        int goalsAllowed = Integer.parseInt(record.get("Goals Allowed"));
+
+        return new FootBallData(goals, goalsAllowed);
     }
 }
