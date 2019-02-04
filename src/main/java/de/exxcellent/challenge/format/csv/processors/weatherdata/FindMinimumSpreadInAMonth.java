d353e279f29f3fc90d3c30ee450421f34f88e032
Denis <denisschulze@gmx.net>
2019-02-04 19:54:02 +0100

    add weather data processor

--- src/main/java/de/exxcellent/challenge/format/csv/processors/weatherdata/FindMinimumSpreadInAMonth.java
+++ src/main/java/de/exxcellent/challenge/format/csv/processors/weatherdata/FindMinimumSpreadInAMonth.java
@@ -8,6 +8,20 @@ import java.util.List;
 public class FindMinimumSpreadInAMonth implements CsvProcessor<WeatherData, Integer> {
     @Override
     public Integer process(List<WeatherData> data) {
-        return 0;
+        int currentSpread = 0;
+        int currentMinSpread = 0;
+
+        WeatherData dayWithMinSpread = data.get(0);
+
+        for (WeatherData weatherData: data.subList(1, data.size())) {
+
+            currentSpread = weatherData.getMaxTemp() - weatherData.getMinTemp();
+            currentMinSpread = dayWithMinSpread.getMaxTemp() - dayWithMinSpread.getMinTemp();
+
+            if (currentSpread < currentMinSpread) {
+                dayWithMinSpread = weatherData;
+            }
+        }
+        return dayWithMinSpread.getDay();
     }
 }
