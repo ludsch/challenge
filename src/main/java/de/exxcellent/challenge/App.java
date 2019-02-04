399ff858337062728ad42c0ed5181b1eb9d4e370
Denis <denisschulze@gmx.net>
2019-02-04 19:56:44 +0100

    add weather challenge solution

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,5 +1,12 @@
 package de.exxcellent.challenge;
 
+import de.exxcellent.challenge.format.csv.CsvReader;
+import de.exxcellent.challenge.format.csv.data.definition.WeatherData;
+import de.exxcellent.challenge.format.csv.data.mapper.WeatherDataCsvRecordMapper;
+import de.exxcellent.challenge.format.csv.processors.weatherdata.FindMinimumSpreadInAMonth;
+
+import java.io.IOException;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -15,9 +22,16 @@ public final class App {
     public static void main(String... args) {
 
         // Your preparation code …
-
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
+        CsvReader<WeatherData, Integer> weatherDataCsvReader = new CsvReader<>(new WeatherDataCsvRecordMapper());
+        try {
+            weatherDataCsvReader.parse("/de/exxcellent/challenge/weather.csv");
+        } catch (IOException e) {
+            System.out.printf("%s%n", e.getMessage());
+        }
+        String dayWithSmallestTempSpread = String.valueOf(weatherDataCsvReader.process(new FindMinimumSpreadInAMonth()));
         System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
+
+
 
         String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
         System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
