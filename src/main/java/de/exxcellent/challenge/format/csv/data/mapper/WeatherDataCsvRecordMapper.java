35876f9a405c4cc87ce5b623a65555a3e111394c
Denis <denisschulze@gmx.net>
2019-02-04 18:03:57 +0100

    add weather data record mapper

---
+++ src/main/java/de/exxcellent/challenge/format/csv/data/mapper/WeatherDataCsvRecordMapper.java
@@ -0,0 +1,12 @@
+package de.exxcellent.challenge.format.csv.data.mapper;
+
+import de.exxcellent.challenge.format.csv.data.definition.WeatherData;
+
+import java.util.Map;
+
+public class WeatherDataCsvRecordMapper implements CsvRecordMapper<WeatherData> {
+    @Override
+    public WeatherData map(Map<String, String> record) {
+        return null;
+    }
+}
