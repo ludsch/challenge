950645c32b4186a0b7a6347c558a37b482a3d3e9
Denis <denisschulze@gmx.net>
2019-02-04 17:45:33 +0100

    implement csv parsing

--- src/main/java/de/exxcellent/challenge/format/csv/CsvReader.java
+++ src/main/java/de/exxcellent/challenge/format/csv/CsvReader.java
@@ -1,13 +1,59 @@
 package de.exxcellent.challenge.format.csv;
 
+import de.exxcellent.challenge.format.csv.data.mapper.CsvRecordMapper;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.io.FilenameUtils;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 
 public class CsvReader<T, U> {
 
     private List<T> rows = new ArrayList<>();
 
+    private CsvRecordMapper<T> csvRecordMapper;
 
+
+    public void parse(String filePath) throws IOException {
+        // Each parse flushes data
+        if (getRows().size() > 0) {
+            rows.clear();
+        }
+
+        Reader in = null;
+
+        try {
+
+            if (!FilenameUtils.getExtension(filePath).equals("csv")) {
+                throw new InputMismatchException();
+            }
+
+            in = new InputStreamReader(getClass().getResourceAsStream(filePath));
+            Iterable<CSVRecord> records = CSVFormat.DEFAULT.withHeader().parse(in);
+
+            for (CSVRecord record: records) {
+                addRow(csvRecordMapper.map(record.toMap()));
+            }
+
+            in.close();
+
+        } catch (IOException e) {
+            System.out.printf("%s%n", e.getMessage());
+            throw e;
+
+        } catch (InputMismatchException e) {
+            System.out.printf("%s%n", e.getMessage());
+            throw e;
+
+        } finally {
+            in.close();
+        }
+    }
 
 
     public List<T> getRows() {
