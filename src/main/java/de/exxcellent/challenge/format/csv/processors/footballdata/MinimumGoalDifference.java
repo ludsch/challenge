035fc5d1e1c027e1def05be8b55bbdd04ab62b37
Denis <denisschulze@gmx.net>
2019-02-04 20:19:11 +0100

    add football processor skeleton

---
+++ src/main/java/de/exxcellent/challenge/format/csv/processors/footballdata/MinimumGoalDifference.java
@@ -0,0 +1,13 @@
+package de.exxcellent.challenge.format.csv.processors.footballdata;
+
+import de.exxcellent.challenge.format.csv.data.definition.FootBallData;
+import de.exxcellent.challenge.format.csv.processors.CsvProcessor;
+
+import java.util.List;
+
+public class MinimumGoalDifference implements CsvProcessor<FootBallData, String> {
+    @Override
+    public String process(List<FootBallData> data) {
+        return null;
+    }
+}
