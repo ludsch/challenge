308234ef780e0422526f37b92479b066d38bc229
Denis <denisschulze@gmx.net>
2019-02-04 21:24:57 +0100

    add football solution to main class

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,8 +1,11 @@
 package de.exxcellent.challenge;
 
 import de.exxcellent.challenge.format.csv.CsvReader;
+import de.exxcellent.challenge.format.csv.data.definition.FootBallData;
 import de.exxcellent.challenge.format.csv.data.definition.WeatherData;
+import de.exxcellent.challenge.format.csv.data.mapper.FootBallDataCsvRecordMapper;
 import de.exxcellent.challenge.format.csv.data.mapper.WeatherDataCsvRecordMapper;
+import de.exxcellent.challenge.format.csv.processors.footballdata.MinimumGoalDifference;
 import de.exxcellent.challenge.format.csv.processors.weatherdata.FindMinimumSpreadInAMonth;
 
 import java.io.IOException;
@@ -20,11 +23,12 @@ public final class App {
      * @param args The CLI arguments passed
      */
     public static void main(String... args) {
+        final String CHALLENGE_DATA_FILE_PATH = "/de/exxcellent/challenge/";
 
-        // Your preparation code …
+        // Weather data solution
         CsvReader<WeatherData, Integer> weatherDataCsvReader = new CsvReader<>(new WeatherDataCsvRecordMapper());
         try {
-            weatherDataCsvReader.parse("/de/exxcellent/challenge/weather.csv");
+            weatherDataCsvReader.parse(CHALLENGE_DATA_FILE_PATH + "weather.csv");
         } catch (IOException e) {
             System.out.printf("%s%n", e.getMessage());
         }
@@ -32,8 +36,15 @@ public final class App {
         System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
 
 
+        // Football data solution
+        CsvReader<FootBallData, String> footBallDataCsvReader = new CsvReader<>(new FootBallDataCsvRecordMapper());
+        try {
+            footBallDataCsvReader.parse(CHALLENGE_DATA_FILE_PATH + "football.csv");
+        } catch (IOException e) {
+            System.out.printf("%s%n", e.getMessage());
+        }
 
-        String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
+        String teamWithSmallestGoalSpread = footBallDataCsvReader.process(new MinimumGoalDifference());
         System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
     }
 }
